b5de2add74bd41c6f1ea7cbcd1a06fb3af033593
Amirhossien <52884154+samirhosseinn@users.noreply.github.com>
2022-11-06 20:41:20 +0330

    add images and custom style

---
+++ registration_app/lib/style/images.dart
@@ -0,0 +1,8 @@
+class Images{
+  Images._();
+
+  static const String login = "Assets/login.jpg";
+  static const String register = "Assets/register.png";
+  static const String forgotPassword = "Assets/forgot-password.jpg";
+  static const String confrimEmail = "Assets/confrim-email.jpg";
+}
---
+++ registration_app/lib/style/style.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+import 'package:registration_app/Responsive/size_config.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  // -------------------------  whiteText --------------------------------
+  static var whiteText = TextTheme(
+    headline1: _headline1White,
+    headline2: _headline2White,
+    headline3: _headline3White,
+    headline4: _headline4White,
+    headline5: _headline5White,
+    headline6: _headline6White,
+    button: _buttonWhite,
+    subtitle1: _subtitle1White,
+    subtitle2: _subtitle2White,
+  );
+  static final TextStyle _headline1White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline2White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 4.75 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline3White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 4.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline4White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 4 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline5White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 3.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline6White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 3 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _buttonWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 2.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle1White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 2 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle2White = TextStyle(
+    color: Colors.white,
+    fontFamily: "IranSans",
+    fontSize: 1.5 * SizeConfig.textMultiplier!,
+  );
+  // -------------------------  whiteText --------------------------------
+
+  // -------------------------  BlackText --------------------------------
+  static var blackText = TextTheme(
+    headline1: _headline1Black,
+    headline2: _headline2Black,
+    headline3: _headline3Black,
+    headline4: _headline4Black,
+    headline5: _headline5Black,
+    headline6: _headline6Black,
+    button: _buttonBlack,
+    subtitle1: _subtitle1Black,
+    subtitle2: _subtitle2Black,
+  );
+  static final TextStyle _headline1Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline2Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 4.75 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline3Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 4.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline4Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 4 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline5Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 3.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline6Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 3 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _buttonBlack = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 2.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle1Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 2 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle2Black = TextStyle(
+    color: Colors.black,
+    fontFamily: "IranSans",
+    fontSize: 1.5 * SizeConfig.textMultiplier!,
+  );
+  // -------------------------  BlackText --------------------------------
+
+  // -------------------------  GreyText --------------------------------
+  static var greyText = TextTheme(
+    headline1: _headline1Grey,
+    headline2: _headline2Grey,
+    headline3: _headline3Grey,
+    headline4: _headline4Grey,
+    headline5: _headline5Grey,
+    headline6: _headline6Grey,
+    button: _buttonGrey,
+    subtitle1: _subtitle1Grey,
+    subtitle2: _subtitle2Grey,
+  );
+  static final TextStyle _headline1Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline2Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 4.75 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline3Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 4.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline4Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 4 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline5Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 3.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline6Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 3 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _buttonGrey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 2.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle1Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 2 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle2Grey = TextStyle(
+    color: Colors.grey,
+    fontFamily: "IranSans",
+    fontSize: 1.5 * SizeConfig.textMultiplier!,
+  );
+  // -------------------------  GreyText --------------------------------
+
+
+
+// -------------------------  whiteText --------------------------------
+  static var whiteEnglishText = TextTheme(
+    headline1: _headline1EnglishWhite,
+    headline2: _headline2EnglishWhite,
+    headline3: _headline3EnglishWhite,
+    headline4: _headline4EnglishWhite,
+    headline5: _headline5EnglishWhite,
+    headline6: _headline6EnglishWhite,
+    button: _buttonEnglishWhite,
+    subtitle1: _subtitle1EnglishWhite,
+    subtitle2: _subtitle2EnglishWhite,
+  );
+  static final TextStyle _headline1EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline2EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 4.75 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline3EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 4.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline4EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 4 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline5EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 3.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _headline6EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 3 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _buttonEnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 2.5 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle1EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 2 * SizeConfig.textMultiplier!,
+  );
+  static final TextStyle _subtitle2EnglishWhite = TextStyle(
+    color: Colors.white,
+    fontFamily: "Arial",
+    fontSize: 1.5 * SizeConfig.textMultiplier!,
+  );
+  // -------------------------  whiteText --------------------------------
+
+
+}
